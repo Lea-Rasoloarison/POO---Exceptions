2127f96560606b6ce1b8746de2da08cd64561c58
Lea Rasoloarison <100602775+Lea-Rasoloarison@users.noreply.github.com>
2022-04-12 10:05:23 +0200

    initialisation
    initialisation

---
+++ car.php
@@ -0,0 +1,37 @@
+<?php
+
+class Car
+{
+    private bool hasParkBrake();
+
+    public function parkBrake(boolean $brake)
+    {
+        if (hasParkBrake === true){
+            throw new Exception('Homer le frein à main !!');
+        }
+        return "false";
+    }
+
+    try{
+        parkBrake = 
+}catch(Exception $e){
+var_dump($e);
+}
+
+echo 'coucou';
+}
+
+
+public function setParkBrake(bool $currentBrake):
+{
+    if($currentSpeed >= 0) {
+        $this->currentSpeed = $currentSpeed;
+    }
+}
+
+public function start(): string // Définition de FORWARD
+{
+    $this->currentSpeed = 50;
+
+    return "Go !";
+}
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once 'Car.php';
+
+
+// Visualisation de MotorWay
+echo "MotorWay".'</br>';
+
+
+
+
+
+
+
+
+
